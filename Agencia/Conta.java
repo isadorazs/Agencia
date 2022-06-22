b9080bf7ec1eaf7cdd3e535d1e8322d81539a44c
Isadora Stéfany <53875866+isadorazs@users.noreply.github.com>
2022-06-22 18:50:46 +0000

    atualização

--- Agencia/Conta.java
+++ Agencia/Conta.java
@@ -1,19 +1,25 @@
 public class Conta
 {
-    private int codigo;
+    private int codigoConta;
     private int tipo;
     private double saldo;
     private double limite;
+    private double sacar;
+    private double depositar;
+    private double transferir;
     
     
 public Conta() {
+    
 
+
+  
 }
-    public int getcodigo(int codigo) {
-		    return codigo;
+    public int getcodigoConta(int codigoConta) {
+		    return codigoConta;
 	}
-     public void setcodigo(int codigo) {
-		this.codigo = codigo;
+     public void setcodigoConta(int codigo) {
+		this.codigoConta = codigoConta;
 	}
      public int gettipo() {
 		return tipo;
@@ -22,7 +28,7 @@ public Conta() {
 		this.tipo = tipo;
 	
 	}
-	
+    
      public double getSaldo(double saldo){
                  this.saldo = saldo;
                  return saldo;
@@ -32,6 +38,28 @@ public Conta() {
                 return limite;
         
         }
+
+    public void sacar(double valor){
+        
+      
+        if(saldo >= valor){
+            saldo -= valor;
+            //saques++;
+            System.out.println("Sacado: " + valor);
+            System.out.println("Novo saldo: " + saldo + "\n");
+        } else {
+            System.out.println("Saldo insuficiente. Faça um depósito\n");
+        }
+    }
+
+    public void depositar(double valor){ 
+      
+        saldo += valor;
+        System.out.println("Depositado: " + valor);
+        System.out.println("Novo saldo: " + saldo + "\n");
+    }
+    
+
 }
 
     
